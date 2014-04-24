55d5e9df3b1550659cea0d358ad3194b809a131c
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2014-04-24 17:05:19 +0530

    added edit form

--- offices/urls.py
+++ offices/urls.py
@@ -1,3 +1,6 @@
 from django.conf.urls import patterns, include, url
+from .views import OfficeDetailsView
 
-urlpatterns = patterns('', )
+urlpatterns = patterns('', 
+	url(r'officedetails/(?P<id>\d+)/$', OfficeDetailsView.as_view(), name="Office Details"),
+)
--- offices/views.py
+++ offices/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+# Create your views here.
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+
+from django.views.generic import ListView, View, UpdateView
+from django.shortcuts import render
+from .models import Office
+from django.http.response import HttpResponse, HttpResponseRedirect
+from braces.views import LoginRequiredMixin, AjaxResponseMixin, JSONResponseMixin
+
+
+class OfficeDetailsView(DetailView):
+	template_name = 'office_details.html'
+	model = Office
+
+	def get_object(self):
+		return Office.objects.get(id=self.kwargs['id'])
